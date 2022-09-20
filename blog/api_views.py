6cdbec761b5ce2b6f2b18b221e70d6ae990e9a70
Kayode Lawal <lawalkeyd@gmail.com>
2022-09-20 20:35:35 +0000

    Fixed double return

--- blog/api_views.py
+++ blog/api_views.py
@@ -34,7 +34,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
 
     if request.method == "GET":
-        return return JsonResponse(PostSerializer(post).data)
+        return JsonResponse(PostSerializer(post).data)
     elif request.method == "PUT":
         post_data = json.loads(request.body)
         serializer = PostSerializer(post, data=post_data)
